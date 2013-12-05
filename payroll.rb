f88b38e9bb2a2f933dd5a11dda774c411a94b81d
Jonah <jk51287@gmail.com>
2013-12-05 15:23:23 -0500

    Added Load/Sales classes

--- payroll.rb
+++ payroll.rb
@@ -5,61 +5,65 @@ TAX_RATE = 0.30
 class Employee
   attr_accessor :first_name, :last_name, :job, :salary, :commission, :bonuses, :quota
 
-  def initalize(filename)
-    @file = IO.read(filename)
-    @file = CSV.parse("#{@file}")
+  def initalize(information)
+    @first_name = first_name
+    @last_name = last_name
+    @job = job
+    @salary = salary
+    # @file = IO.read('employee_payroll.csv')
+    # @file = CSV.parse("#{@file}")
   end
-  
-  def list_of_employees
+
+  def gross_salary
+    @salary.to_f / 12
+  end
+
+  def taxes
+    gross_salary * TAX_RATE
+  end
+
+  def net_pay
+    gross_salary - taxes
+  end
+end
+
+class Load
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def read_employee
     @employees_list = []
-    CSV.foreach('employee_payroll.csv', headers: true) do |row|
-      # read all information for each employee
-      @first_name = filename["first_name"]
-      @last_name = filename["last_name"]
-      @job = filename["job"]
-      @salary = filename["salary"].to_f
-      @commission = filename.fetch["commission", 0]
-      @bonuses = filename.fetch["bonuses", 0]
-      @quota = filename.fetch["quota", 0]
+    CSV.foreach(@filename, headers:true) do |row|
+      information = row.to_hash
+      if information["job"] == 'Salary_Only'
+        @employees_list << Employee.new(information)
+      elsif information["job"] == 'Quota'
+        @employees_list << QuotaSalesPerson.new(information)
+      elsif information["job"] == 'Commission'
+        @employees_list << CommissionSalesPerson.new(information)
+      elsif information["job"] == 'Owner'
+        @employees_list << Owner.new(information)
+      end
+    end
+    @employees_list
+  end
+end
 
-      # pass the information into a hash for each employee
-      @employee = { first_name: first_name, last_name: last_name, job: job, salary: salary, commission: commission, bonuses: bonuses, quota: quota }
-      @employees_list << @employee
+class Sales
+  def list_of_sales
+    sales = []
+    CSV.foreach('sales.csv', headers: true) do |row|
+      sales << row
     end
   end
-
-    def gross_salary
-      @salary.to_f / 12
-    end
-
-    def taxes
-      gross_salary * TAX_RATE
-    end
-
-    def net_pay
-      gross_salary - taxes
-    end
 end
 
 
 
-    # CSV.foreach(@filename, headers:true) do |row|
-    #   data = row.to_hash
-    #   if data["employee_type"] == 'salary_only'
-    #     @employees << Employee.new(data)
-    #   elsif data["employee_type"] == 'quota'
-    #     @employees << QuotaSalesPerson.new(data)
-    #   elsif data["employee_type"] == 'commission'
-    #     @employees << CommissionSalesPerson.new(data)
-    #   elsif data["employee_type"] == 'owner'
-    #     @employees << Owner.new(data)
-    #   end
+# test = Employee.new
+# binding.pry
+# puts test
 
-
-
-    # @employees = []
-    # CSV.foreach('employee_payroll.csv', headers:true) do |row|
-
-
-person = Employee.new('employee_payroll.csv')
-worker = person.
+    # person = Employee.new('employee_payroll.csv')
+    # worker = person.
