438edf54678717cbe0febc778266964eb200c8a8
Jonah <jk51287@gmail.com>
2013-12-06 11:43:02 -0500

    Work up to 12/6. Start of Friday and using 2 CSVs

--- payroll.rb
+++ payroll.rb
@@ -3,7 +3,7 @@ require 'csv'
 
 TAX_RATE = 0.30
 class Employee
-  attr_accessor :first_name, :last_name, :job, :salary, :commission, :bonuses, :quota
+  attr_reader :first_name, :last_name, :job, :salary #:commission, :bonuses, :quota
 
   def initalize(information)
     @first_name = first_name
@@ -25,14 +25,12 @@ class Employee
   def net_pay
     gross_salary - taxes
   end
-end
 
-class Load
-  def initialize(filename)
-    @filename = filename
-  end
+  # def self.load_csv(filename)
+  
+  # end
 
-  def read_employee
+  def self.read_employee
     @employees_list = []
     CSV.foreach(@filename, headers:true) do |row|
       information = row.to_hash
@@ -50,7 +48,16 @@ class Load
   end
 end
 
-class Sales
+
+class Load
+  def initialize(filename)
+    @filename = filename
+  end
+  Employee.read_employee
+end
+
+#convert to sale object
+class Sale
   def list_of_sales
     sales = []
     CSV.foreach('sales.csv', headers: true) do |row|
@@ -58,6 +65,48 @@ class Sales
     end
   end
 end
+
+class CommissionSalesPerson < Employee
+    attr_reader  :commission
+  def initialize(information)
+    @commission = information["Commission"].to_f
+    @gross_monthly_sales = information["gross_sale_value"].to_f
+    super(information)
+  end
+
+  def commission_calc
+    sum = 0
+    if employee_payroll[:last_name] == sales[:last_name]
+      sales.each do |money|
+        money = sales[:gross_sale_value]
+        sum += money
+      end
+    end
+    sum
+  end
+  
+  # set last names = to each other
+  # if e
+  #  get sales[:gross_sale_value]
+  # sum up GSV
+  # do math
+
+
+
+  # read and parse sales.csv
+  # iterate over sales.csv, check last name of commission jobs
+  # if job == 'commision', get last_name and gross_sales_value for EACH employee
+  # sum gross_sales_value PER employee
+  # check gross_sales_value
+
+  def gross_salary
+    new_commission = @salary.to_f / 12 + @commission * @gross_monthly_sales
+  end
+end
+
+# loads everything from csv file yay
+
+# employees = Employee.load_csv('employee_payroll.csv')
 
 
 
